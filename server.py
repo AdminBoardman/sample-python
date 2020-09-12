52e7c2e57e8e1720abbce54d44ce05edd8bc7046
Joseph Cusack <joe@Josephs-iMac.lan>
2020-09-12 13:32:21 -0400

    tes msg

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello my friend Bella Marias! you requested %s' % (self.path)
+        msg = 'Hi Bella Marias! you requested %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
